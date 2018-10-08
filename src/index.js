469609951ef6da193b154ca0a7a41b35a18a6dff
Anzhelika <anzhelika110k@gmail.com>
2018-10-08 18:32:15 +0300

    refactor: replace var to let

--- src/index.js
+++ src/index.js
@@ -1,20 +1,17 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
-		this.equation = equation;
-	var arr = this.equation.split(" ");
-	var a = arr[0],
-		b = arr[3].concat(arr[4]),
-		x = arr[6],
-		c = arr[7].concat(arr[8]);
+	const arr = equation.split(" ");
+	const a = arr[0];
+	const b = arr[3].concat(arr[4]);
+	const c = arr[7].concat(arr[8]);
 
-	var D = eval(b * b - 4 * a * c),
-		x1 = Math.round(eval((-b + Math.sqrt(D))/(2*a))),
-		x2 = Math.round(eval((-b - Math.sqrt(D))/(2*a)));
-	
-	var sqrt_arr = [];
-	sqrt_arr.push(x1, x2);
-	sqrt_arr.sort(function(a, b){
-		return a-b
-	})
-	return sqrt_arr
-}
+	const D = eval(b ** 2 - 4 * a * c);
+	const x1 = Math.round(eval((-b + Math.sqrt(D)) / (2 * a)));
+	const x2 = Math.round(eval((-b - Math.sqrt(D)) / (2 * a)));
+
+	const sqrtArr = [];
+	sqrtArr.push(x1, x2);
+	sqrtArr.sort(function (a, b) {
+		return a - b
+	});
+	return sqrtArr;
+};
